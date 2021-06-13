c1b3341fc9a12c9b6045f85f94b729592b59ea00
PeterCamilleri <peter.c.camilleri@gmail.com>
2021-06-13 19:16:39 -0400

    Fleshed out comments

--- src/ram_defs.asm
+++ src/ram_defs.asm
@@ -7,9 +7,9 @@
 ram_start:
 
 __here:     reserve 2   ; The next byte to be used in the dictionary
-__last:     reserve 2   ; The last word to be defined.
-__current:  reserve 2   ; The word currently being defined.
-__mode:     reserve 1   ; The current language mode.
+__last:     reserve 2   ; The LFA of the last word to be defined.
+__current:  reserve 2   ; The LFA of the word currently being defined.
+__mode:     reserve 1   ; The current mode. 0 is interpret else compiling.
 
 free_start:
 
